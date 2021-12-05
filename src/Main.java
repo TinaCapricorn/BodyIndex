18cf5144ff976c91b1b85ae56b85970e39a1c126
Klein Kristina <klein281293@gmail.com>
2021-12-05 18:22:39 +0700

    Fix2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BodyIndex service = new BodyIndex();
+        bodyIndex service = new bodyIndex();
         int kg = 70;
         double height = 1.65;
         double index = service.calculate(kg,height);
--- src/bodyIndex.java
+++ src/bodyIndex.java
@@ -1,4 +1,4 @@
-public class BodyIndex {
+public class bodyIndex {
     public double calculate (int kg, double height){
         double index = kg / Math.pow(height, 2);
         return index;
